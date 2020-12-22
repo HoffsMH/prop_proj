73a413169b4666f1daa74ddeee1711507239a1db
HoffsMH <matthecker@pm.me>
2020-12-22 13:18:20 -0600

    initial commit

---
+++ lib/prop_proj.ex
@@ -0,0 +1,18 @@
+defmodule PropProj do
+  @moduledoc """
+  Documentation for PropProj.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PropProj.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/prop_proj_test.exs
@@ -0,0 +1,8 @@
+defmodule PropProjTest do
+  use ExUnit.Case
+  doctest PropProj
+
+  test "greets the world" do
+    assert PropProj.hello() == :world
+  end
+end
